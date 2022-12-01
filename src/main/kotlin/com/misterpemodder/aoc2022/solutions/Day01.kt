c47538c2671a72cc4f05d179c5965c0dae0ee59c
Yanis Guaye <yguaye44@gmail.com>
2022-12-01 16:59:29 +0100

    Day01 solution

---
+++ src/main/kotlin/com/misterpemodder/aoc2022/solutions/Day01.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2022 Yanis Guaye
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.misterpemodder.aoc2022.solutions
+
+import com.misterpemodder.aoc2022.Solution
+import com.misterpemodder.aoc2022.utf8Lines
+import io.ktor.utils.io.*
+import it.unimi.dsi.fastutil.longs.LongArrayList
+import it.unimi.dsi.fastutil.longs.LongComparators
+import it.unimi.dsi.fastutil.longs.LongList
+import kotlinx.coroutines.flow.fold
+
+internal object Day01 : Solution<LongList> {
+    override val year = 2022
+    override val day = 1
+
+    override suspend fun setup(input: ByteReadChannel): LongList {
+        var elfCalories = 0L
+        val allCalories = input.utf8Lines().fold(LongArrayList()) { caloriesGroups, line ->
+            if (line.isNotEmpty()) {
+                elfCalories += line.toLong()
+            } else {
+                caloriesGroups += elfCalories
+                elfCalories = 0
+            }
+            caloriesGroups
+        }
+
+        allCalories.sort(LongComparators.OPPOSITE_COMPARATOR)
+        return allCalories
+    }
+
+    override fun part1(data: LongList): Long = data.getLong(0)
+
+    override fun part2(data: LongList): Long = data.getLong(0) + data.getLong(1) + data.getLong(2)
+}
