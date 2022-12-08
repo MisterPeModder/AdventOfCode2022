e84cca45138652a7c7cdb47b62d156af9521d0d6
Yanis Guaye <yguaye44@gmail.com>
2022-12-08 12:18:59 +0100

    Add Boolean to Int utility function

---
+++ src/main/kotlin/com/misterpemodder/aoc2022/Util.kt
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2022 Yanis Guaye
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.misterpemodder.aoc2022
+
+/**
+ * Converts this boolean to an integer.
+ *
+ * @returns 1 if true, 0 if false
+ */
+internal fun Boolean.toInt(): Int = if (this) {
+    1
+} else {
+    0
+}
